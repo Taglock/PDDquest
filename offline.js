acb4b8f23601820345a7f7e9d6dbd64192546955
Taglock <39584594+Taglock@users.noreply.github.com>
2022-04-26 01:44:03 +0500

    initial comit

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1650919340,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/sprite-sheet1.png",
+		"images/sprite-sheet2.png",
+		"images/char1-sheet0.png",
+		"images/button-sheet0.png",
+		"images/btn.png",
+		"images/sprite2-sheet0.png",
+		"images/sprite2-sheet1.png",
+		"images/sprite3-sheet0.png",
+		"media/deliberate thought - kevin macleod.m4a",
+		"media/deliberate thought - kevin macleod.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"dial1.txt",
+		"dial.txt"
+	]
+}
